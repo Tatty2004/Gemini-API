51f9e02ce1e0374116bd0a5cbd0f9c1254d3392c
panda41983 <165868910+panda41983@users.noreply.github.com>
2024-04-07 14:59:09 -0700

    small changes

--- beefriendjava.js
+++ beefriendjava.js
@@ -1,7 +1,7 @@
 // export API_KEY = AIzaSyCZVU4WBA9-W7uTAGFosex95I6gaUqVgbc
 //npm install @google/generative-ai
 let slider = document.getElementById("myRange");
-let output = document.getElementById("demo");
+let output = document.getElementById("val");
 //let summ = document.getElementById("summaryArea");
             
 output.innerHTML = slider.value;
@@ -12,6 +12,6 @@ slider.oninput = function() {
 
 function clicked() {
    //console.log("Summarizing..."); 
-   document.getElementById("summaryArea").innerHTML = "Beefriend: Summarizing...";
+   document.getElementById("summaryArea").innerHTML = "Beefriend:";
                 
 }
