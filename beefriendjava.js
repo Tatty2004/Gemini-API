41d356cfbb675ff62a24e7c2158c7b520e198a5d
panda41983 <165868910+panda41983@users.noreply.github.com>
2024-04-12 22:25:56 -0700

    more

--- beefriendjava.js
+++ beefriendjava.js
@@ -10,8 +10,59 @@ slider.oninput = function() {
    output.innerHTML = this.value;
 }
 
-function clicked() {
-   //console.log("Summarizing..."); 
-   document.getElementById("summaryArea").innerHTML = "Beefriend:";
-                
-}
+document.getElementById("summarize").addEventListener("click", () => {
+    document.getElementById("summaryArea").innerHTML = "SUMMARY HERE";
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("#input").addEventListener("keydown", function(e) {
+      if (e.code === "Enter") {
+        let input = inputField.value;
+        inputField.value = "";
+        output(input);
+      }
+    });
+  });
+
+
+function output(input) {
+    let product;
+    let text = input.toLowerCase().replace(/[^\w\s\d]/gi, "");
+    text = text
+      .replace(/ a /g, " ")
+      .replace(/whats/g, "what is")
+      .replace(/please /g, "")
+      .replace(/ please/g, "");
+   
+    if (compare(utterances, answers, text)) {
+      product = compare(utterances, answers, text);
+    } 
+    else {
+      product = alternatives[Math.floor(Math.random() * alternatives.length)];
+    }
+   
+    //update  DOM
+    addChatEntry (input, product);
+  }
+  function addChatEntry(input, product) {
+    const messagesContainer = document.getElementById("messages");
+    
+    let userDiv = document.createElement("div");
+    userDiv.id = "user";
+    userDiv.className = "user response";
+    userDiv.innerHTML = `${input}`;
+    messagesContainer.appendChild(userDiv);
+   
+    let botDiv = document.createElement("div");
+    let botText = document.createElement("span");
+    botDiv.id = "bot";
+    botDiv.className = "bot response";
+    botText.innerText = "Typing...";
+    botDiv.appendChild(botText);
+    messagesContainer.appendChild(botDiv);
+   
+                        
+   
+    setTimeout(() => {
+      botText.innerText = `${product}`;
+    }, 2000); }
--- simpleinput.js
+++ simpleinput.js
@@ -16,8 +16,20 @@ userInterface.on("line", async input => {
   // For text-only input, use the gemini-pro model
   const model = genAI.getGenerativeModel({ model: "gemini-pro"});
 
+  
+
   const result = await model.generateContent(input);
   const response = await result.response;
   const text = response.text();
   console.log(text);
 })
+//function clicked() {
+  //console.log("Summarizing..."); 
+  //chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+  // let url = tabs[0].url;
+    
+  //});
+ // document.getElementById("summaryArea").innerHTML = "summary";
+  
+               
+//}
